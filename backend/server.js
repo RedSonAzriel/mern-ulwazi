400a74f7541ca5044138c875a1181659eb5b217f
RedSonAzriel <redseanational@gmail.com>
2022-02-13 21:23:00 -0500

    built api get request, having trouble connecting to port 5000.

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
-console.log('Hello World')
+const express = require('express')
+const dotenv = require('dotenv').config()
+
+//creating the port we want to use. the env.Port allows us to access the port variable anywhere else in the backend. 
+const port = process.env.PORT || 5000
+
+//initializing express 
+const app = express()
+
+//Get api - sending a request (req) and a receiveing a response (res) variable
+app.get('/api/ulwazi', (req, res ) => {
+    res.send('Ulwazi is live')
+})
+
+//listening event for the port technique
+app.listen(port, () => console.log(`Server started on port ${port}`))
