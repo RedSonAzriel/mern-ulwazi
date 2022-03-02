1fe7bd9bc2d7ae7094c2dd38a87ba48b717ed5fc
RedSonAzriel <redseanational@gmail.com>
2022-03-01 20:40:44 -0500

    started working on the front end, Installed Redux Toolkit, and React for the router

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+//Came with initial install but don't need to use. 
+// import React from 'react';
+// import logo from './logo.svg';
+// import { Counter } from './features/counter/Counter';
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Ulwazi</h1>
+    </div>
+  );
+}
+
+export default App;
